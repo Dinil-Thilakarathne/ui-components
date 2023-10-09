dea92bba276f1564b7ab08abdc1f56ab3fd195c8
Dinil Thilakarathne <sonacode44@gmail.com>
2023-10-09 10:28:51 +0530

    add new project profile-card

---
+++ profile-card/components/Profilecard.jsx
@@ -0,0 +1,41 @@
+import {FaLocationDot, FaPhone} from 'react-icons/fa6';
+
+const Profilecard = ({profileImgUrl, profileName , ProfileAddress, ProfileOccupation, ProfileNumber, ProfileEmail}) => {
+    return (
+        <div className="card">
+            <div className="card-top">
+                <div className="card-top__profile-img">
+                    <img src={profileImgUrl} alt="" />
+                </div>
+                <div className="card-top__profile-content">
+                    <h2 className="profile-name">
+                        {profileName}
+                    </h2>
+                    <h4 className="profile-occupation">
+                        {ProfileOccupation}
+                    </h4>
+                    <div className="profile-info">
+                        <div className="profile-address">
+                            <FaLocationDot/>
+                            <span>
+                                {ProfileAddress}
+                            </span>
+                        </div>
+                        <div className="profile-number">
+                            <FaPhone/>
+                            <span>
+                                {ProfileNumber}
+                            </span>
+                        </div>  
+                    </div>
+                </div>
+            </div>
+            <div className="card-bottom">
+                <div className="divider"></div>
+                <p className="profile-email">{ProfileEmail}</p>
+            </div>
+        </div>
+    )
+}
+
+export default Profilecard
---
+++ profile-card/src/App.jsx
@@ -0,0 +1,28 @@
+import Profilecard from "../components/Profilecard"
+import { logo } from "../public"
+import './style.scss'
+
+const App = () => {
+    const profileData = {
+        imgUrl : logo,
+        name : 'Dinil',
+        occupation: 'Front-end Developer',
+        address: 'Lorem ipsum dolor',
+        number: '111-222-3333',
+        email: 'example@gmail.com'
+    }
+    return (
+        <>
+            <Profilecard
+                profileImgUrl={profileData.imgUrl}
+                profileName={profileData.name}
+                ProfileOccupation={profileData.address}
+                ProfileAddress={profileData.address}
+                ProfileNumber={profileData.number}
+                ProfileEmail={profileData.email}
+            />
+        </>
+    )
+}
+
+export default App
