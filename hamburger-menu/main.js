3856fef58a9be58cb646f0e3dd0d2600cb924b8e
Dinil Thilakarathne <sonacode44@gmail.com>
2024-01-31 19:12:08 +0530

    upload new files

---
+++ hamburger-menu/main.js
@@ -0,0 +1,43 @@
+import './style.scss'
+
+const topnavOpenBtn = document.querySelector('#btnOpen')
+const media = window.matchMedia('(width < 40em)');
+const topNavMenu = document.querySelector('.topnav__menu');
+
+let isOpen = false
+
+function handleBtnClick() {
+  isOpen = !isOpen
+  topnavOpenBtn.setAttribute('aria-expanded', isOpen.toString());
+
+  if (isOpen) {
+    topNavMenu.removeAttribute('inert');
+    topNavMenu.removeAttribute('style');
+  }
+}
+
+function setupTopNav(e) {
+  if (e.matches) {
+    // is mobile
+    console.log('is mobile');
+    topNavMenu.setAttribute('inert', '');
+    topNavMenu.style.transition = 'none';
+  } else {
+    // is tablet/desktop
+    console.log('is desktop');
+    topNavMenu.removeAttribute('inert');
+
+    setTimeout(() => {
+      isOpen = false
+      topnavOpenBtn.setAttribute('aria-expanded', isOpen.toString())
+    }, 500);
+  }
+}
+
+setupTopNav(media);
+
+topnavOpenBtn.addEventListener('click', handleBtnClick)
+
+media.addEventListener('change', function (e) {
+  setupTopNav(e);
+});
